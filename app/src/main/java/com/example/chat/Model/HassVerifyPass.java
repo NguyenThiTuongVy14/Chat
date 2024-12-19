576c12e18d97dae2432d0ec7536de9e93aa0f5a6
appchat <appchat@gmail.com>
2024-12-19 23:38:17 +0700

    last

---
+++ app/src/main/java/com/example/chat/Model/HassVerifyPass.java
@@ -0,0 +1,11 @@
+package com.example.chat.Model;
+
+public class HashPass {
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean verifyPassword(String password, String hashedPassword) {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+}
